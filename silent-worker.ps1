faf57c9cb3bc003cb96a43ad40f5bdd168f57fd0
Max <71935156+MaXfAa@users.noreply.github.com>
2022-02-28 21:13:38 +0100

    works now

--- silent-worker.ps1
+++ silent-worker.ps1
@@ -1,3 +1,9 @@
-New-ItemProperty -Path "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\GameDVR" -Name "AllowgameDVR" -Value "0" -PropertyType "DWord"
-New-ItemProperty -Path "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" -Name "AppCaptureEnabled" -Value "0" -PropertyType "DWord"
-New-ItemProperty -Path "HKEY_CURRENT_USER\System\GameConfigStore" -Name "GameDVR_Enabled" -Value "0" -PropertyType "DWord"
+New-Item –Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" –Name "GameDVR"
+Set-Itemproperty -path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR' -Name 'AllowgameDVR' -Value '0'
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" -Name "AllowgameDVR" -Value "0" -PropertyType "DWord"
+
+Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\GameDVR' -Name 'AppCaptureEnabled' -Value '0'
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\GameDVR" -Name "AppCaptureEnabled" -Value "0" -PropertyType "DWord"
+
+Set-Itemproperty -path 'HKCU:\System\GameConfigStore' -Name 'GameDVR_Enabled' -Value '0'
+New-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name "GameDVR_Enabled" -Value "0" -PropertyType "DWord"
